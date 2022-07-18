d9fce54a77dea8ce192cb9e2a669b7941994b699
EzDC-MP <77631876+EzDC-MP@users.noreply.github.com>
2022-07-18 16:07:52 +0200

    Delete example-test directory

--- example-test/resources/lrs_properties.py
+++
@@ -1,9 +0,0 @@
-"""
-Contains user-specific information for testing.
-"""
-
-
-endpoint="http://garimpeiro12.loria.fr/trax/ws/xapi/statements"
-version ="1.0.1" # 1.0.1 | 1.0.0 | 0.95 | 0.9
-username="testsuite"
-password="password"
